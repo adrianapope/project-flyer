153360810920efd97ad9fae71235219fedb9181b
Adriana Pope <adrianagiancarla@gmail.com>
2015-11-03 17:37:15 +0700

    add photo model

---
+++ app/Photo.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Photo extends Model
+{
+	protected $table = 'flyer_photos';
+
+	protected $fillable = ['path'];
+
+
+	/**
+	* A photo belongs to a flyer.
+	*
+	* @returns \Illuminate\Database\Eloquent\Relations\BelongsTo
+	*/
+    public function flyer()
+    {
+    	return $this->belongsTo('App\Flyer');
+    }
+}

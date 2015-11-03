423dff11c25f4e6dd01190fa1a66e61c0091c739
Adriana Pope <adrianagiancarla@gmail.com>
2015-11-03 17:37:29 +0700

    add FlyersController

---
+++ app/Http/Controllers/FlyersController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Requests\FlyerRequest;
+use App\Http\Controllers\Controller;
+
+class FlyersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('flyers.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  FlyerRequest $request
+     * @return Response
+     */
+public function store(FlyerRequest $request)
+    {
+        // validate the form
+        // this is done through FlyerRequest not here
+
+        // persist the flyer
+        // that'll be an array that maps to the columns
+        App\Flyer::create($request->all());
+
+        // redirect to landing page
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}

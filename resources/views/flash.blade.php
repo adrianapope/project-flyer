a63bdbea30ca4d4d9c7f83b4a18c286428084c6a
Adriana Pope <adrianagiancarla@gmail.com>
2015-11-05 16:18:36 +0700

    make level dynamic. added an overlay script

--- resources/views/flash.blade.php
+++ resources/views/flash.blade.php
@@ -3,9 +3,20 @@
 	    swal({
 	      title: "{{ session('flash_message.title') }}",
 	      text: "{{ session('flash_message.message') }}",
-	      type: "info",
+	      type: "{{ session('flash_message.level') }}",
 	      timer: 1700,
 	      confirmButtonText: false
+	   	});
+ 	</script>
+@endif
+
+@if (session()->has('flash_message_overlay'))
+	<script type="text/javascript">
+	    swal({
+	      title: "{{ session('flash_message_overlay.title') }}",
+	      text: "{{ session('flash_message_overlay.message') }}",
+	      type: "{{ session('flash_message_overlaay.level') }}",
+	      confirmButtonText: 'Okay',
 	   	});
  	</script>
 @endif

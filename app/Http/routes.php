d46314522b3e40b497f7473125a1704551ed5e26
Adriana Pope <adrianagiancarla@gmail.com>
2015-11-05 23:10:34 +0700

    addPhoto route which will track down a flyer using its zip and street

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,5 +17,11 @@ Route::get('/', function () {
 
 Route::resource('flyers', 'FlyersController');
 Route::get('{zip}/{street}', 'FlyersController@show');
+Route::post('{zip}/{street}/photos', 'FlyersController@addPhoto');
 
 //projectflyer.com/90808/5529-east-keynote-street
+
+// we want to use zip street to track down a specific flyer.
+// if i do a post request to that it will create or store a new photo that
+// is associated with this flyer
+//zip/street/photos  example: /flyers/1/photos

78b593d68a7a37357706e6d50174ed1a0a89af55
Adriana Pope <adrianagiancarla@gmail.com>
2015-11-05 16:17:26 +0700

    updated helpers function

--- app/helpers.php
+++ app/helpers.php
@@ -1,15 +1,15 @@
 <?php
 
 /**
-* Method I call in FlyersController. Pass in message.
-* We fetch the flash class out of the container. Which is our file Flash.php
-* And then we call a message method on it.
-* the message method is just where you call session flash and pass in key and value.
-* so in this context, we just pass in the message that we send from the controller.
+* Grabs flash and defaults to the level of info.
 */
-function flash($title, $message)
+function flash($title = null, $message = null)
 {
 	$flash = app('App\Http\Flash');
 
-	return $flash->message($title, $message);
+	if (func_num_args() == 0) {
+		return $flash;
+	}
+
+	return $flash->info($title, $message);
 }

1d5bbd470de27b33975d611edd9d5daef52a8136
Adriana Pope <adrianagiancarla@gmail.com>
2015-11-05 17:37:59 +0700

    added a scope

--- app/Flyer.php
+++ app/Flyer.php
@@ -22,6 +22,24 @@ class Flyer extends Model
 	];
 
 	/**
+	* Scope query to those located at a given address.
+	*
+	* @param Builder $query
+	* @param string $zip
+	* @param string $street
+	* @return Builder
+	*/
+	public function scopeLocatedAt($query, $zip, $street)
+	{
+		// replace spaces with a dash
+        $street = str_replace('-', ' ', $street);
+
+        // query where zip is this and street is that
+        return $query->where(compact('zip', 'street'));
+    }
+
+
+	/**
 	* A flyer is composed of many photos.
 	*
 	* @return \Illuminate\Database\Eloquent\Relations\HasMany

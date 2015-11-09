037b66fa39e78883370beb58148dd5af90f24c5b
Adriana Pope <adrianagiancarla@gmail.com>
2015-11-09 14:14:19 +0700

    added authentication routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,13 +15,22 @@ Route::get('/', function () {
     return view('pages.home');
 });
 
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
 Route::resource('flyers', 'FlyersController');
 Route::get('{zip}/{street}', 'FlyersController@show');
 // give this a named route
 // Route::post('{zip}/{street}/photos', 'FlyersController@addPhoto');
 Route::post('{zip}/{street}/photos', ['as' => 'store_photo_path', 'uses' => 'FlyersController@addPhoto']);
-//projectflyer.com/90808/5529-east-keynote-street
 
+//projectflyer.com/90808/5529-east-keynote-street
 // we want to use zip street to track down a specific flyer.
 // if i do a post request to that it will create or store a new photo that
 // is associated with this flyer

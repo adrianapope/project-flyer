c045307a7aa360cd33a3d562df7728a66e5dfb16
Adriana Pope <adrianagiancarla@gmail.com>
2015-11-09 13:22:27 +0700

    adjusted bootstrap columns in create and form views

--- resources/views/flyers/create.blade.php
+++ resources/views/flyers/create.blade.php
@@ -3,21 +3,17 @@
 @section('content')
 <h1>Selling your home?</h1>
 
-<div class="row">
-	<div class="col-md-6">
-		<form method="POST" action="/flyers" enctype="multipart/form-data">
-			@include('flyers.form')
+<form method="POST" action="/flyers" enctype="multipart/form-data">
+	@include('flyers.form')
 
-			@if (count($errors) > 0)
-				<div class="alert alert-danger">
-					<ul>
-						@foreach ($errors->all() as $error)
-							<li>{{ $error }}</li>
-						@endforeach
-					</ul>
-				</div>
-			@endif
-		</form>
-	</div>
-</div>
+	@if (count($errors) > 0)
+		<div class="alert alert-danger">
+			<ul>
+				@foreach ($errors->all() as $error)
+					<li>{{ $error }}</li>
+				@endforeach
+			</ul>
+		</div>
+	@endif
+</form>
 @stop

64f64efbac69ed6bbd79463981f0879660690a09
Adriana Pope <adrianagiancarla@gmail.com>
2015-11-10 15:12:50 +0700

    temporary fix to route - issue resolved of not finding port 8000/flyers (currently flyer/create) for store method

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,37 +1,22 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
+# General
 Route::get('/', function () {
     return view('pages.home');
 });
 
-// Authentication routes...
+# Authentication
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
-// Registration routes...
+# Registration
 Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
 
-Route::resource('flyers', 'FlyersController');
+# Flyers
+Route::post('/flyer/create', 'FlyersController@store'); // temp
+Route::resource('/flyers', 'FlyersController');
 Route::get('{zip}/{street}', 'FlyersController@show');
-// give this a named route
-// Route::post('{zip}/{street}/photos', 'FlyersController@addPhoto');
 Route::post('{zip}/{street}/photos', ['as' => 'store_photo_path', 'uses' => 'FlyersController@addPhoto']);
 
-//projectflyer.com/90808/5529-east-keynote-street
-// we want to use zip street to track down a specific flyer.
-// if i do a post request to that it will create or store a new photo that
-// is associated with this flyer
-//zip/street/photos  example: /flyers/1/photos

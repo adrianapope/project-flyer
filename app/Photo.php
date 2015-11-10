f37c09c932d9e6da8edd5314b9d91c40bb2e8c82
Adriana Pope <adrianagiancarla@gmail.com>
2015-11-10 15:12:21 +0700

    added fillable fields to Photo, imported Image class up top, and changed fromForm to named(). added a move() method

--- app/Photo.php
+++ app/Photo.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use App\Flyer;
+use Image;
 use Illuminate\Database\Eloquent\Model;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
@@ -11,7 +12,7 @@ class Photo extends Model
 {
 	protected $table = 'flyer_photos';
 
-	protected $fillable = ['path'];
+	protected $fillable = ['path', 'name', 'thumbnail_path'];
 
 	// default base path for all of our photos
 	protected $baseDir = 'flyers/photos';
@@ -29,23 +30,62 @@ class Photo extends Model
 
 
 	/**
-	* It will accept an uploaded file instance
-	*
+	* Creates a new instance of photo from a file upload.
+    *
+	* @param string $name
+    * @return self
 	*/
-    public static function fromForm(UploadedFile $file)
+    public static function named($name)
     {
-    	// new up a photo instance (because this is a static constructor)
-    	$photo = new static;
 
-    	// set up a file name. rename file to something unique with a timetamp before the orignal name
-        $name = time() . $file->getClientOriginalName();
+        // creates a new instance of photo and then we name it whatever the file should be
+        // $photo = new static;
+        // return $photo->saveAs($file->getClientOriginalName());
+        return (new static)->saveAs($name);
+    }
 
-        // we can set the path column equal to the baseDir and then the file name.
-    	$photo->path = $photo->baseDir . '/' . $name;
 
-    	// move the file
-    	$file->move($photo->baseDir, $name);
+    /**
+    * Set the proper columns such as name, path and thumbnail_path.
+    *
+    */
+    public function saveAs($name)
+    {
+        // name of the file   12334556777RedHouse.jpg
+        $this->name = sprintf("%s-%s", time(), $name);
 
-    	return $photo;
+        // name of the path   flyers/photos/2342344GreenHouse.jpg
+        $this->path = sprintf("%s/%s", $this->baseDir, $this->name);
+
+        // name of the thumbnail   flyers/photos/tn-123434555BlueHouse.jpg
+        $this->thumbnail_path = sprintf("%s/tn-%s", $this->baseDir, $this->name);
+
+        return $this;
+    }
+
+    /**
+    * Move the file to the baseDir and the name we gave it. And calls makeThumbnail method.
+    *
+    *
+    */
+    public function move(UploadedFile $file)
+    {
+        $file->move($this->baseDir, $this->name);
+
+        $this->makeThumbnail();
+
+        return $this;
+    }
+
+
+    /**
+    * Creates a thumbnail which is fitted and saved.
+    *
+    */
+    public function makeThumbnail()
+    {
+        Image::make($this->path)
+            ->fit(200)
+            ->save($this->thumbnail_path);
     }
 }

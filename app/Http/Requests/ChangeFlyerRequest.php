4b9f632087f63b0df71125103107cf19f34ab5c1
Adriana Pope <adrianagiancarla@gmail.com>
2015-11-11 17:40:09 +0700

    added doc blocks

--- app/Http/Requests/ChangeFlyerRequest.php
+++ app/Http/Requests/ChangeFlyerRequest.php
@@ -10,13 +10,14 @@ use App\Http\Requests\Request;
 * Think of this as representing a user's request to do something.
 * We can represent whether the user is authorized to do this in two different ways.
 * One, general authorization (authorize section) and two validation (the rules section)
-*
-*
+* We can delete the validation in our FlyersController because rules() will take care of it.
+* Since we are in a request object, we can replace $request->zip with $this->zip and so on.
 */
 class ChangeFlyerRequest extends Request
 {
     /**
      * Determine if the user is authorized to make this request.
+     * $this refers to $request
      *
      * @return bool
      */
@@ -25,7 +26,7 @@ class ChangeFlyerRequest extends Request
         return Flyer::where([
             'zip' => $this->zip,
             'street' => $this->street,
-            'user_id' => $this->user()->id
+            'user_id' => $this->user()->id  // same as $request->user()->id
         ])->exists();
     }
 

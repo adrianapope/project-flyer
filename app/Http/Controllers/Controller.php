fae008fdb87b70b8ddf8a4d5a4da62134912a461
Adriana Pope <adrianagiancarla@gmail.com>
2015-11-11 13:17:03 +0700

    added some shortcuts to abstract controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,25 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Auth;
 
 abstract class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected $user;
+
+    public function __construct()
+    {
+    	// declare this equals that
+    	$this->user = Auth::user();
+
+    	// 'blank' equals this piece of code
+    	view()->share('signedIn', Auth::check());
+
+    	// blank' equals this piece of code
+		// view()->share('user', Auth::user());
+		view()->share('user', $this->user);
+
+    }
 }

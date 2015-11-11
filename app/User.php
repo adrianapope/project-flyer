5be44e9a2a7d51a5d67f8ed1d5772dfddf277523
Adriana Pope <adrianagiancarla@gmail.com>
2015-11-11 17:31:34 +0700

    added reverse relationship from User to Flyer

--- app/User.php
+++ app/User.php
@@ -36,4 +36,15 @@ class User extends Model implements AuthenticatableContract,
      * @var array
      */
     protected $hidden = ['password', 'remember_token'];
+
+    /**
+    * A user can have many flyers.
+    *
+    */
+    public function flyers()
+    {
+        return $this->hasMany('App\Flyer');
+    }
 }
+
+
